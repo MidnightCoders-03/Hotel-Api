083dae4409916407a1b7fc47afd442c8727fd411
M Burak Yilmazer <mbyilmazer38@gmail.com>
2024-03-30 15:55:35 +0300

    totalPrice

--- src/models/reservation.js
+++ src/models/reservation.js
@@ -1,38 +1,42 @@
 "use strict";
 
-
 const {
   mongoose: { Schema, model },
 } = require("../configs/dbConnection");
-
 
 const ReservationSchema = new Schema(
   {
     userId: {
       type: Schema.Types.ObjectId,
       ref: "User",
-      required: true
+      required: true,
     },
 
     roomId: {
       type: Schema.Types.ObjectId,
       ref: "Room",
-      required: true
+      required: true,
     },
 
     arrival_date: {
       type: Date,
-      required: [true,"Please enter your arrival_date in YEAR-MONTH-DAY format"]
+      required: [
+        true,
+        "Please enter your arrival_date in YEAR-MONTH-DAY format",
+      ],
     },
 
     departure_date: {
       type: Date,
-      required: [true,"Please enter your departure_date in YEAR-MONTH-DAY format"]
+      required: [
+        true,
+        "Please enter your departure_date in YEAR-MONTH-DAY format",
+      ],
     },
 
     guest_number: {
       type: Number,
-      default: 1
+      default: 1,
     },
 
     night: {
@@ -41,17 +45,16 @@ const ReservationSchema = new Schema(
       // set: function (){
       //     return Number(this.departure_date.split("-")[2]) - Number(this.arrival_date.split("-")[2])
       // }  //! if user makes a reservation in a month
-    set: function(){
-        const arrival = new Date(this.arrival_date) //! arrival_date in milliseconds
-        const departure = new Date(this.departure_date) //! departure_date in milliseconds
-        const difference = departure - arrival
+      set: function () {
+        const arrival = new Date(this.arrival_date); //! arrival_date in milliseconds
+        const departure = new Date(this.departure_date); //! departure_date in milliseconds
+        const difference = departure - arrival;
 
-        const millisecondsPerDay = 1000 * 60 * 60 * 24 //! milliseconds in a day
-        const night = Math.floor ( difference / millisecondsPerDay) //! calculate the night as a day
-         
-        return night
+        const millisecondsPerDay = 1000 * 60 * 60 * 24; //! milliseconds in a day
+        const night = Math.floor(difference / millisecondsPerDay); //! calculate the night as a day
 
-    }
+        return night;
+      },
     },
 
     price: {
@@ -60,22 +63,22 @@ const ReservationSchema = new Schema(
       // enum: [100, 150, 200, 300],
     },
 
-    totalprice: {
+    totalPrice: {
       type: Number,
       // required: true,
-    //   set: function(){
-    //     if(this.guest_number == 1){
-    //           return this.totalprice = this.price.enum[0]
-    //     }else if(this.guest_number == 2){
-    //           return this.totalprice = this.price.enum[1] 
-    //     }else if(this.guest_number == 4){
-    //         return this.totalprice = this.price.enum[2]
-    //     }else if(this.guest_number > 4 ){
-    //         return this.totalprice = this.price.enum[3]
-    //     }else {
-    //         throw new Error("Please enter a valid number")
-    //     }
-    // }
+      //   set: function(){
+      //     if(this.guest_number == 1){
+      //           return this.totalprice = this.price.enum[0]
+      //     }else if(this.guest_number == 2){
+      //           return this.totalprice = this.price.enum[1]
+      //     }else if(this.guest_number == 4){
+      //         return this.totalprice = this.price.enum[2]
+      //     }else if(this.guest_number > 4 ){
+      //         return this.totalprice = this.price.enum[3]
+      //     }else {
+      //         throw new Error("Please enter a valid number")
+      //     }
+      // }
     },
   },
   {
@@ -84,4 +87,4 @@ const ReservationSchema = new Schema(
   }
 );
 
-module.exports = model("Reservation", ReservationSchema) 
+module.exports = model("Reservation", ReservationSchema);
