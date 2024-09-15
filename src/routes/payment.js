6685fd1417b88eb1daec39c6d865e6613e43d588
Serkan <s8ersis@gmail.com>
2024-09-15 21:49:50 +0300

    next version user model and sync files handled left in comment

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -4,6 +4,6 @@ const router = require("express").Router()
 
 const payment = require("../controllers/payment")
 
-router.post('/create', payment);
+router.post('/create', payment.paymentInfo);
 
 module.exports = router;
